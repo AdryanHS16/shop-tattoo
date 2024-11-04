9e4d3f3f85865a222c457a381908449bb67cd3e2
Adryan Henrique <hsadryan16@gmail.com>
2024-11-04 13:13:08 -0300

    chore: finishing home page

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,21 +1,42 @@
+"use client";
+
 import Image from "next/image";
 import logo from "@/app/_assets/Logo.svg";
 import { londrinaSolid } from "../_lib/fonts";
 import { FaWhatsapp } from "react-icons/fa";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
+  const isActive = (currentPath: string, pathname: string) => {
+    if (currentPath.includes(pathname)) return "underline";
+  };
+
   return (
-    <nav className={`container flex justify-between py-8 ${londrinaSolid.className}`}>
-      <Image src={logo} alt="Logotipo da página" />
+    <nav
+      className={`container flex justify-between py-8 ${londrinaSolid.className}`}
+    >
+      <Link href="/"><Image src={logo} alt="Logotipo da página" /></Link>      
       <ul className="flex gap-6 text-3xl">
-        <li>Serviços</li>
-        <li>Sobre Mim</li>
+        <li className={`${isActive(pathname, "servicos")}`}>
+          <Link href="/servicos">Serviços</Link>
+        </li>
+        <li className={`${isActive(pathname, "sobre")}`}>
+          <Link href="/sobre">Sobre Mim</Link>
+        </li>
         <li className="text-right">
-          Contato <br />
-          <span className="text-xs text-zinc-400 font-extralight flex gap-1 items-center">
-            <FaWhatsapp />
-            (+55 62 99999 9999)
-          </span>
+          <Link href="/contato">
+            <span className={`${isActive(pathname, "contato")}`}>
+              Contato
+              <br />
+            </span>
+            <span className="text-xs text-zinc-400 font-extralight flex gap-1 items-center">
+              <FaWhatsapp />
+              (+55 62 99999 9999)
+            </span>
+          </Link>
         </li>
       </ul>
     </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+import { londrinaSolid } from "./_lib/fonts";
+import portrait from "@/app/_assets/Retrato.png";
+
 export default function Home() {
   return (
-    <>
-      <main></main>
-    </>
+    <main className="relative flex-1">
+      <h1
+        className={`absolute -z-10 text-[400px] uppercase text-zinc-800 left-0 top-0 -left-20 leading-tight ${londrinaSolid.className}`}
+      >
+        Tattoo
+      </h1>
+      <div className="font-black text-zinc-600 text-8xl pt-56 uppercase">
+        <h2>inspire-se</h2>
+        <h2>espresse-se</h2>
+        <h2 className="text-yellow-400">tatue-se</h2>
+      </div>
+      <Image
+        className="w-96 absolute right-0 -bottom-20"
+        src={portrait}
+        alt="Retrato de Sandro Luz"
+      />
+    </main>
   );
 }
